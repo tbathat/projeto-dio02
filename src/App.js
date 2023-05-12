896fbbe86aaf889665055b1c49ff0b467895894c
tbathat <tabathapradier@gmail.com>
2023-05-12 10:18:40 -0300

    Set count example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useState, useEffect } from "react";
+
 const App = () => {
+  const [usuario, setUsuario] = useState("");
+  const [count, setCount] = useState(0);
+  const [usuarios, setUsuarios] = useState(["Tabatha", "Luana", "Laura"]);
+
+  const handleAddUser = () => {
+    setUsuarios([...usuarios, usuario]);
+    setUsuario("");
+  };
+
+  useEffect(() => {
+    setCount(usuarios.length);
+  }, [usuarios]);
+
   return (
     <div className="App">
-      <h1>Hello DIO!</h1>
+      <h1>Hello, Dio!</h1>
+      <h3>Total: {count}</h3>
+      <div>
+        <input
+          value={usuario}
+          onChange={(event) => setUsuario(event.target.value)}
+        />
+        <button onClick={handleAddUser}>Adicionar</button>
+      </div>
+      <hr />
+
+      {usuarios.map((item) => (
+        <p>{item}</p>
+      ))}
     </div>
   );
 };
